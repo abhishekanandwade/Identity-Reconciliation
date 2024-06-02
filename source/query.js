e80e524c3a5d9b36746aa3b2be1f3e61de99f8e9
Abhishek <abhi.anandwade@gmail.com>
2024-06-02 20:50:37 +0530

    optimised secondary contact entry if primary contact already exists

--- source/query.js
+++ source/query.js
@@ -5,21 +5,6 @@ import { client } from "../index.js";
 async function userQueryExec(body) {
     console.log('User Query Execution##############', body);
 
-    // query to get the user details
-    // const query = {
-    //     text: 'INSERT INTO contact (phone_number, email,link_precedence, created_at,updated_at) VALUES($1, $2, $3, $4,$5)',
-    //     values: [body.phoneNumber, body.email, 'primary', new Date(), new Date()],
-    // };
-
-    // // execute the query
-    // client.query(query, (err, res) => {
-    //     if (err) {
-    //         console.error(err.stack);
-    //     } else {
-    //         console.log(res.rows[0]);
-    //     }
-    // });
-
     try {
         await client.query('BEGIN');
 
@@ -29,40 +14,44 @@ async function userQueryExec(body) {
         console.log('Select all entries---------', selectContactResult.rows);
 
         if (selectContactResult.rows.length > 0) {
+            let linkedId;
 
             let primaryContact = selectContactResult.rows.filter(contact => contact.link_precedence === 'primary');
             if (primaryContact.length === 0) {
                 primaryContact = selectContactResult.rows.filter(contact => contact.link_precedence === 'secondary' && contact.linked_id != null);
+                linkedId = primaryContact[0].linked_id;
+            } else {
+                linkedId = primaryContact[0].id;
             }
-            console.log('Primary Contact---------', primaryContact);
-            const linkedId = primaryContact[0].id;
+            console.log('Primary Contact---------', primaryContact, linkedId);
 
-            const secondaryContactQuery = `
-    WITH params AS (
-        SELECT
-            $1 AS phone_number,
-            $2 AS email,
-            $3::link_precedence AS link_precedence,
-            $4::integer AS linked_id,
-            $5::TIMESTAMPTZ AS created_at,
-            $6::TIMESTAMPTZ AS updated_at
-    )
-    INSERT INTO contact (phone_number, email, link_precedence, linked_id ,created_at, updated_at)
-    SELECT phone_number, email, link_precedence, linked_id, created_at, updated_at FROM params
-    WHERE NOT EXISTS (
-        SELECT 1 FROM contact WHERE phone_number = (SELECT phone_number FROM params) AND email = (SELECT email FROM params)
-    )
-    RETURNING *`;
+            try {
+                const secondaryContactQuery = `
+                    INSERT INTO contact (phone_number, email, link_precedence, linked_id ,created_at, updated_at)
+                    VALUES ($1, $2, $3::link_precedence, $4::integer, $5::TIMESTAMPTZ, $6::TIMESTAMPTZ)
+                    RETURNING *`;
 
-            const secondaryContactResult = await client.query(secondaryContactQuery, [body.phoneNumber, body.email, 'secondary', linkedId, new Date().toISOString(), new Date().toISOString()]);
+                const secondaryContactResult = await client.query(secondaryContactQuery, [body.phoneNumber, body.email, 'secondary', linkedId, new Date().toISOString(), new Date().toISOString()]);
 
-            console.log('secondary entry---------', secondaryContactResult.rows);
+                console.log('secondary entry---------', secondaryContactResult.rows);
+            } catch (error) {
+                if (error.code === '23505' && (error.constraint === 'unique_phone_where_email_null' || error.constraint === 'unique_email_where_phone_null')) {
+                    // Ignore the error
+                } else {
+                    // Handle the error
+                    throw error;
+                }
+            }
         } else {
             const primaryContactsQuery = 'Insert into contact (phone_number, email, link_precedence, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *';
             const primaryContactsResult = await client.query(primaryContactsQuery, [body.phoneNumber, body.email, 'primary', new Date().toISOString(), new Date().toISOString()]);
 
             console.log('Primary entry!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', primaryContactsResult.rows);
         }
+
+        
+
+        console.log('here to continue@@@@@@@@@@@@@@@@@@@@@@@@@')
 
         // if (primaryContactResult.rows.length > 0) {
         //   const primaryContact = primaryContactResult.rows[0];
